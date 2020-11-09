fdccecb5d4bef644dd4d426442fc8d6099fcb1ff
rgonzalezrdam1 <rgonzalezrdam1@gmail.com>
2020-11-09 19:53:07 +0100

    Http para obtener las incidencias y drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:info_carreteras_navarra/screens/listincidencias_screen.dart';
 import 'package:info_carreteras_navarra/screens/listincidencias_swiper_screen.dart';
 import 'package:info_carreteras_navarra/screens/listview_screen.dart';
@@ -8,7 +9,7 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Incidencias Carreteras Navarra',
       home: ListIncidenciasSwiperScreen(),
       debugShowCheckedModeBanner: false,
